c97f2a040d6861b75a7441421d87f81971f49a99
Fernando Basso <fernandobasso.br@gmail.com>
2024-02-09 08:53:12 -0300

    Update ch03 deps

---
+++ statemgntbook/ch03/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from './02a_context_static_value';
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper />
+    </div>
+  );
+}
+
+export default App;
--- statemgntbook/ch03/src/index.tsx
+++ statemgntbook/ch03/src/index.tsx
@@ -1,14 +1,16 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
