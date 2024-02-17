2a0eaf716bf5cba33f04eb3d2ba538cf23341306
Fernando Basso <fernandobasso.br@gmail.com>
2024-02-17 08:28:38 -0300

    Update context propagation example

--- statemgntbook/ch03/src/04a_context_propagation.tsx
+++ statemgntbook/ch03/src/04a_context_propagation.tsx
@@ -7,17 +7,18 @@ import {
   useState,
 } from "react";
 
+const log = console.log.bind(console);
+
 const ColorContext = createContext("black");
 
 function useColorContext() {
-  const ctx = useContext(ColorContext);
-  return ctx;
+  return useContext(ColorContext);
 }
 
 //
 // This components uses the context.
 //
-function Color() {
+function Color({ name }: { name: string}) {
   const color = useColorContext();
   const renderCount = useRef(1);
 
@@ -27,6 +28,7 @@ function Color() {
 
   return (
     <div style={{ color }}>
+      <h2>{name}</h2>
       <p>Color: {color}</p>
       <p>Renders: {renderCount.current}</p>
     </div>
@@ -34,50 +36,57 @@ function Color() {
 }
 
 //
-// Based on the base component that consumes the context. It doesn't
+// Based on the base component that consumes the context. It doesn"t
 // matter that it is memoized. If context changes, it will rerender.
 //
 const ColorMemoized = memo(Color);
 
 //
-// Not consuming the context.
+// Not consuming the context. If a parent rerenders, it also rerenders.
 //
-function Dummy() {
+function Dummy({ name }: { name: string }) {
   const renderCount = useRef(1);
+  log(`<${name} />`);
 
   return (
     <div>
+      <h2>{name}</h2>
       <p>Renders: {renderCount.current}</p>
     </div>
   );
 }
 
 //
-// Memoized, and not consuming the context.
+// Memoized, and not consuming the context. Even if parent rerenders, it
+// will NOT rerender (unless its own props change then) because it is
+// memoized.
 //
 const DummyMemoized = memo(Dummy);
 
 function Parent() {
+  log("<Parent />");
+
   return (
     <ul>
       <li>
-        <Color />
+        <Color name="Color" />
       </li>
       <li>
-        <ColorMemoized />
+        <ColorMemoized name="ColorMemoized" />
       </li>
       <li>
-        <Dummy />
+        <Dummy name="Dummy" />
       </li>
       <li>
-        <DummyMemoized />
+        <DummyMemoized name="DummyMemoized" />
       </li>
     </ul>
   );
 }
 
 function MyApp() {
-  const [color, setColor] = useState("red");
+  const [color, setColor] = useState("orange");
+  log("<MyApp />");
 
   return (
     <ColorContext.Provider value={color}>
